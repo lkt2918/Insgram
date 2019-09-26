39979d3f11c4c8d55afa1ceb5328c5f1c059aa5e
李可汀 <liketing@liketingdeAir.attlocal.net>
2019-09-26 00:16:09 -0500

    You are the best shooter

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(blank = True, null = True)
+    image = ProcessedImageField(
+        upload_to = 'static/image/posts',
+        format = 'JPEG',
+        blank = True,
+        null = True,
+    )
+
+
--- Insta/urls.py
+++ Insta/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from Insta.views import HelloDjango
+from Insta.views import PostView, PostDetail
 
 
 urlpatterns = [
-    path('', HelloDjango.as_view(), name ='home'),
+    path('', PostView.as_view(), name ='home'),
+    path('post/<int:pk>/', PostDetail.as_view(), name= 'post_detail')
 ]
